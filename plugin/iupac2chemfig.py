9b8252516ad1b190a1ab043ffff3048096a8d977
Moritz F. Kuntze <martllcraft@gmail.com>
2017-07-30 21:33:34 +0200

    Fixed some indentation/line breaking issues

--- plugin/iupac2chemfig.py
+++ plugin/iupac2chemfig.py
@@ -6,19 +6,23 @@ line = vim.current.line
 
 def normal(str):
     vim.command("normal "+str)
+    
+vim.command("set paste")
 
 try:
     iupacname = re.search('\\chemfig{(.*)}', line).group(1)
     smiles =  commands.getoutput('echo "' + iupacname + '" > /tmp/name.txt; java -jar ~/javalibs/opsin.jar /tmp/name.txt /tmp/out.txt >/dev/null 2>&1; cat /tmp/out.txt')
     if smiles != "":
         if mode == "normal":
-            chemfig = commands.getoutput('mol2chemfig -of -i direct "' + smiles + '"')
+            chemfig = commands.getoutput('mol2chemfig -of -i direct "' + smiles + '"') + '\n'
         elif mode == "carbon":
-            chemfig = commands.getoutput('mol2chemfig -cof -i direct "' + smiles + '"')
+            chemfig = commands.getoutput('mol2chemfig -cof -i direct "' + smiles + '"') + '\n'
         else:
             chemfig = iupacname
-        normal('cc'+line.replace(iupacname, chemfig)+'\n')
+        normal('cc'+line.replace(iupacname, chemfig))
     else:
         normal('cc'+line)
 except AttributeError:
     normal('cc'+line)
+
+vim.command("set nopaste")
